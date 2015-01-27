df7943d46eeb61542586ca66cb5ffec7aeb3daec
Alex Curtis <alex@alexandercurtis.net>
2015-01-27 21:16:55 +0000

    First attempt at counting tags

--- src/html_stats/core.clj
+++ src/html_stats/core.clj
@@ -1,24 +1,23 @@
 (ns html-stats.core
   (:require [clojure.java.io :as io]
-            [net.cgrand.enlive-html :as html]))
+            [net.cgrand.enlive-html :as html]
+            [clojure.zip :as z]))
 
 
 (defn parse-html-file []
   (with-open [re (io/reader "test/html_stats/sample.html")]
     (html/html-resource re)))
 
-(defn count-tags
-  [node]
-  )
+(defn has-children [node] (and (map? node) (contains? node :content)))
 
 (defn analyse
   [parsed-html]
-  {:number-of-tags (count-tags parsed-html)
+  {:number-of-tags (frequencies (remove nil? (map :tag (tree-seq has-children :content {:content parsed-html}))))
    :foo 99})
 
 (defn -main
   "Reads an html file and gives you some stats."
-  [x]
+  [& args]
   (let [parsed-html (parse-html-file)
         stats (analyse parsed-html)]
     (clojure.pprint/pprint {:stats stats})))
